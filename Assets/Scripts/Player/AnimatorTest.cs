35f3db5611e6f7aab2ea427f366161e2ee75653a
Deltric <collmor78945@learn.flvs.net>
2019-06-24 17:52:40 -0500

    Initial Commit

---
+++ Assets/Scripts/Player/AnimatorTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class AnimatorTest : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator charAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            charAnimator.SetBool("IsDead", true);
+            charAnimator.Update(0);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            charAnimator.SetBool("IsDead", false);
+            charAnimator.Update(0);
+        }
+    }
+
+}
